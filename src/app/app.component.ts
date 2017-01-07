7230b529eb056febfc4be38f77c91318bffcdee9
abhiit89 <abhi.aec89@gmail.com>
2017-01-08 01:48:29 +0530

    feat(app): Added Support for Immutable JS and D3 JS
    Configured the app component to use the Immutable JS constructs and Installed the D3 JS Dependencies
    
    No Breaking Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
-
+import {Component} from '@angular/core';
+import {List, Record} from 'immutable';
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+    title = 'app works!';
+    demoRecord = Record({
+        property:'defaultValue',
+        index:0,
+        works:true,
+        valueList:List([])
+    });
+
+    data = this.demoRecord();
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
-import { TodoListComponent } from './todo-list/todo-list.component';
-import { TodoItemComponent } from './todo-item/todo-item.component';
-
 @NgModule({
   declarations: [
-    AppComponent,
-    TodoListComponent,
-    TodoItemComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
